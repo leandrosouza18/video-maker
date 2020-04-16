a4ff8eeb0ff49e3a8e11a2daf51a50f2474bd90e
leandrosouza18 <leandrosouza1808@hotmail.com>
2020-04-16 19:31:48 -0300

    start text robot

---
+++ credentials/Readme.md
@@ -0,0 +1,11 @@
+# Credentials format
+
+## Algorithmia
+
+File: `algorithmia.json`
+
+```
+{
+    "apiKey" : "simD4NpvqjqKcsmYANgODrUtNJT1"
+}
+```
--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const readline = require('readline-sync');
-
-function start(){
+const robots   = {
+	text: require('./robots/text.js')
+}
+async function start(){
 	const content = {};
 
 	content.searchTerm = askAndReturnSearchTerm();
-	content.prefix = askAndReturnPrefix();
+	content.prefix     = askAndReturnPrefix();
+
+	await robots.text(content);
 
 	function askAndReturnSearchTerm(){
 		return readline.question("Type a wikipedia search term: ")
 	}
 
 	function askAndReturnPrefix(){
-		const prefixes 			  = ["who is", "What is", "The histpry of"];
+		const prefixes 			  = ["Who is", "What is", "The history of"];
 		const selectedPrefixIndex = readline.keyInSelect(prefixes, "Choose one option: ");
 		const selectedPrefixText  = prefixes[selectedPrefixIndex]; 
 		return selectedPrefixText;
